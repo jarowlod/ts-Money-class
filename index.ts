ba0801f89cafd40b3983b0975387fc8443ae83bd
jarowlod <jarowlod@gmail.com>
2023-01-27 15:07:04 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -75,22 +75,22 @@ function number2words(n){
     var digit = n%10;
     if (n < 100) return tens[~~(n/10)-2] + (digit? " " + num[digit]: "");
     if (n < 1000) return hundreds[~~(n/100)] + (n%100 == 0? "": " " + number2words(n%100));
+    
+    const pow = ~~(n/1000000)+1;
+    const baza = ~~(n/(1000**pow));
+    const group = getGroup(baza);
+console.log(n, pow, baza)
+    return number2words(baza) + " " + groups[pow][group] + (n%(1000**pow) != 0? " " + number2words(n%(1000**pow)): "");
+}
 
-    let d = 1000;
-    if (n<1000000) d = 1;
-
-    const baza = ~~(n/1000);
-    const gr = baza%10;
-    console.log(gr, baza)
-    let grr = 0;
-    switch (gr) {
-      case 1: grr = baza<10 ? 0 : 2; break;
-      case 2:
-      case 3:
-      case 4: grr = 1; break;
-      default: grr = 2;
-    };
-    return number2words(~~(n/1000)) + " " + groups[d][grr] + (n%1000 != 0? " " + number2words(n%1000): "");
+function getGroup(baza) {
+  switch (baza%10) {
+    case 1: return baza<10 ? 0 : 2;
+    case 2:
+    case 3:
+    case 4: return 1;
+    default: return 2;
+  };
 }
 
 console.log(number2words(1116321))
