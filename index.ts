1e994baf88d03fe5541edb0c9d2f8170618d1f4d
jarowlod <jarowlod@gmail.com>
2023-01-27 23:27:43 +0100

    money to word PLN

--- index.ts
+++ index.ts
@@ -2,11 +2,13 @@ export class Money {
   constructor(private value: number = 0) {
     this.value = Money.round(isNaN(value) ? 0 : value);
   }
-  
+
   static of(value: number | string): Money {
-    return new Money(typeof value === 'number' ? value.valueOf() : parseFloat(value));
+    return new Money(
+      typeof value === 'number' ? value.valueOf() : parseFloat(value)
+    );
   }
-  
+
   static round(value: number): number {
     return Math.round(value * 100 * (1 + Number.EPSILON)) / 100;
   }
@@ -36,7 +38,7 @@ export class Money {
   }
 
   percent(percent: number = 0): Money {
-    return new Money(this.value * percent / 100);
+    return new Money((this.value * percent) / 100);
   }
 
   valueOf(): number {
@@ -44,53 +46,129 @@ export class Money {
   }
 
   toCurrency(): string {
-    return this.value.toLocaleString('pl-PL', { style: 'currency', currency: 'PLN' });
+    return this.value.toLocaleString('pl-PL', {
+      style: 'currency',
+      currency: 'PLN',
+    });
   }
 }
 
-console.log( 
+console.log(
   new Money(5.015).add(new Money(0.015)).valueOf(),
   Money.of(5.015).add(Money.of(0.015)).valueOf(),
-  Money.of(5.015*100).toCurrency(),
+  Money.of(5.015 * 100).toCurrency(),
   Money.of(3).percent(50).valueOf(),
   Money.floor(5.011),
   Money.of('12kk')
 );
 
+const num = [
+  'zero',
+  'jeden',
+  'dwa',
+  'trzy',
+  'cztery',
+  'pięć',
+  'sześć',
+  'siedem',
+  'osiem',
+  'dziewięć',
+  'dziesięć',
+  'jedenaście',
+  'dwanaście',
+  'trzynaście',
+  'czternaście',
+  'piętnaście',
+  'szesnaście',
+  'siedemnaście',
+  'osiemnaście',
+  'dziewiętnaście',
+];
+const tens = [
+  'dwadzieścia',
+  'trzydzieści',
+  'czterdzieści',
+  'pięćdziesiąt',
+  'sześćdziesiąt',
+  'siedemdziesiąt',
+  'osiemdziesiąt',
+  'dziewięćdziesiąt',
+];
+const hundreds = [
+  '',
+  'sto',
+  'dwieście',
+  'trzysta',
+  'czterysta',
+  'pięćset',
+  'sześćset',
+  'siedemset',
+  'osiemset',
+  'dziewięćset',
+];
 
-var num = ["zero", "jeden", "dwa", "trzy", "cztery", "pięć", "sześć", "siedem", "osiem", "dziewięć", "dziesięć", "jedenaście", "dwanaście", "trzynaście", "czternaście", "piętnaście", "szesnaście", "siedemnaście", "osiemnaście", "dziewiętnaście"];
-var tens = ["dwadzieścia", "trzydzieści", "czterdzieści", "pięćdziesiąt", "sześćdziesiąt", "siedemdziesiąt", "osiemdziesiąt", "dziewięćdziesiąt"];
-var hundreds = ["", "sto", "dwieście", "trzysta", "czterysta", "pięćset", "sześćset", "siedemset", "osiemset", "dziewięćset"];
+const groups = [
+  ['', '', ''],
+  ['tysiąc', 'tysiące', 'tysięcy'],
+  ['milion', 'miliony', 'milionów'],
+  ['miliard', 'miliardy', 'miliardów'],
+  ['bilion', 'biliony', 'bilionów'],
+  ['biliard', 'biliardy', 'biliardów'],
+  ['trylion', 'tryliony', 'trylionów'],
+];
 
-var groups = [["", "", ""],
-            ["tysiąc", "tysiące", "tysięcy"],
-            ["milion", "miliony", "milionów"],
-            ["miliard", "miliardy", "miliardów"],
-            ["bilion", "biliony", "bilionów"],
-            ["biliard", "biliardy", "biliardów"],
-            ["trylion", "tryliony", "trylionów"]];
+const currencyVariation = [' złoty', ' złote', ' złotych'];
 
-function number2words(n){
-    if (n < 20) return num[n];
-    var digit = n%10;
-    if (n < 100) return tens[~~(n/10)-2] + (digit? " " + num[digit]: "");
-    if (n < 1000) return hundreds[~~(n/100)] + (n%100 == 0? "": " " + number2words(n%100));
-    
-    const pow = ~~(n/1000000)+1;
-    const baza = ~~(n/(1000**pow));
-    const group = getGroup(baza);
-console.log(n, pow, baza)
-    return number2words(baza) + " " + groups[pow][group] + (n%(1000**pow) != 0? " " + number2words(n%(1000**pow)): "");
+let sufix = '';
+function inWord(n) {
+  sufix = '';
+  const reszta = ((n - Math.trunc(n)) * 100).toFixed();
+  return number2words(Math.trunc(n)) + ` ${reszta}/100`;
+}
+
+function number2words(n, isSufix = true) {
+  if (n < 1000) sufix = sufix == '' && isSufix ? currencyVariation[getGroup(n)] : sufix;
+  if (n < 20) return num[n] + sufix;
+  var digit = n % 10;
+  if (n < 100) return tens[Math.trunc(n / 10) - 2] + (digit ? ' ' + num[digit] + sufix : sufix);
+  if (n < 1000) return hundreds[Math.trunc(n / 100)] + (n % 100 == 0 ? sufix : ' ' + number2words(n % 100, isSufix));
+
+  const pow = getPow(n);
+  const dzielnik = 1000 ** pow;
+  const baza = Math.trunc(n / dzielnik);
+  const group = getGroup(baza);
+
+  return number2words(baza, false) + ' ' + groups[pow][group] + (n % dzielnik != 0 ? ' ' + number2words(n % dzielnik, true) : currencyVariation[2]);
+}
+
+function getPow(n) {
+  const a = Math.trunc(n / 1000000);
+  for (let i = 1; i <= 5; i++) {
+    if (a < 1000 ** i && a > 0) return ++i;
+  }
+  return 1;
 }
 
 function getGroup(baza) {
-  switch (baza%10) {
-    case 1: return baza<10 ? 0 : 2;
+  switch (baza % 10) {
+    case 1: return baza < 10 ? 0 : 2;
     case 2:
     case 3:
     case 4: return 1;
     default: return 2;
-  };
+  }
 }
 
-console.log(number2words(1116321))
+
+
+
+
+console.log(inWord(2123232100.12));
+
+const val = document.querySelector('#val');
+const div = document.querySelector('#app');
+
+val.addEventListener('input', function () {
+  let n = val.valueAsNumber;
+  div.innerHTML = inWord(parseFloat(n ? n : 0));
+});
